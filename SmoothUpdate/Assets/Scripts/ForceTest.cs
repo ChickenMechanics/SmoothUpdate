7e7a06e5ee8754d34ca31008935f1e4c0e9a4228
ChickenMechanics <ChickenMechanics@gmail.com>
2019-09-15 22:54:14 +0200

    Physics based cubes
    Translated using force

---
+++ SmoothUpdate/Assets/Scripts/ForceTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ForceTest : MonoBehaviour
+{
+    Rigidbody m_Rb;
+
+    public Vector3 m_Dir;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        m_Rb.AddForce(m_Dir, ForceMode.Force);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
