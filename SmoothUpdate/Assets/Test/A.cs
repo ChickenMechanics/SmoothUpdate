76beb81a915d1497ccd4495d867c62c5482804b0
ChickenMechanics <ChickenMechanics@gmail.com>
2019-09-27 23:02:45 +0200

    A

--- SmoothUpdate/Assets/Test/A.cs
+++ SmoothUpdate/Assets/Test/A.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class E : MonoBehaviour
+public class A : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
